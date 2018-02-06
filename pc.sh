7facdda2998a145a57f3ce34943bba1c40f141f9
lpp1985 <409511038@qq.com>
2018-02-06 14:15:00 +0800

    123

---
+++ pc.sh
@@ -0,0 +1,8 @@
+chmod -R 755 ./
+#dos2unix *.*
+git add . -A
+git commit -m 123
+#git push -f Homenas pc:master
+#git push -f Roomnas $1:master
+#git push -f USA $1:master
+git push -f LPP master:master
